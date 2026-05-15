974e89fa949b95efd05da7ab05088b22000f8f52
rexsybimatw <rexsy.bima@mail.com>
2026-05-16 05:42:09 +0700

    applied functiom t load ammo csv

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,84 @@
-use rand;
+use std::error::Error;
 use std::io;
-use std::{error::Error, process};
 
 use serde::Deserialize;
 
 use serde::de::DeserializeOwned;
+
+#[derive(Deserialize, Debug)]
+struct Ammo {
+    #[serde(rename = "~")]
+    id: String,
+
+    #[serde(rename = "pda_encyclopedia_name")]
+    name: String,
+
+    #[serde(rename = "st_data_export_description")]
+    description: String,
+
+    #[serde(rename = "st_prop_weight")]
+    weight: f32,
+
+    #[serde(rename = "st_data_export_ammo_box_size")]
+    box_size: u32,
+
+    #[serde(rename = "st_upgr_cost")]
+    cost: u32,
+
+    #[serde(rename = "BR1")]
+    br1: Option<String>,
+
+    #[serde(rename = "BR2")]
+    br2: Option<String>,
+
+    #[serde(rename = "BR3")]
+    br3: Option<String>,
+
+    #[serde(rename = "BR4")]
+    br4: Option<String>,
+
+    #[serde(rename = "BR5")]
+    br5: Option<String>,
+
+    #[serde(rename = "BR6")]
+    br6: Option<String>,
+
+    #[serde(rename = "BR7")]
+    br7: Option<String>,
+
+    #[serde(rename = "st_data_export_projectiles")]
+    projectiles: u32,
+
+    #[serde(rename = "ui_inv_damage")]
+    damage: f32,
+
+    #[serde(rename = "ui_inv_accuracy")]
+    accuracy: String, // e.g. "66%"
+
+    #[serde(rename = "ui_inv_wrange")]
+    range: String, // e.g. "100%"
+
+    #[serde(rename = "st_data_export_falloff")]
+    falloff: String, // e.g. "82%"
+
+    #[serde(rename = "ui_inv_bspeed")]
+    bullet_speed: String, // e.g. "187%"
+
+    #[serde(rename = "st_data_export_impulse")]
+    impulse: String, // e.g. "55%"
+
+    #[serde(rename = "st_data_export_weapon_degradation")]
+    weapon_degradation: String, // e.g. "40%"
+
+    #[serde(rename = "st_data_export_k_hit")]
+    k_hit: f32,
+
+    #[serde(rename = "st_data_export_k_ap")]
+    k_ap: f32,
+
+    #[serde(rename = "st_data_export_k_air_resistance")]
+    k_air_resistance: f32,
+}
 
 #[derive(Deserialize, Debug)]
 struct Weapon {
@@ -23,13 +97,15 @@ fn read_csv<T: DeserializeOwned>(path: &str) -> Result<Vec<T>, Box<dyn Error>> {
 }
 
 fn main() {
-    todo!("get the field header of the csv file");
-    let csv_file_paths = ["location1", "location2"];
+    // todo!("get the field header of the csv file");
+    let csv_file_paths = ["data/export_ammo.csv", "location2"];
     let app_version: u8 = 1;
     let gamma_tip = "Use bandage to stop the bleeding";
     let gamma_tips = vec!["tip 1", "tip 2", "tip 3"];
     let random_number = rand::random_range(0..gamma_tips.len());
     let mut user_input = String::new();
+    let ammos: Vec<Ammo> = read_csv("data/export_ammo.csv").expect("failed to read csv file");
+    dbg!(ammos);
 
     println!("current app version is {}!", app_version);
     println!("you want to get current gamma tip?");
