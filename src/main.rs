8b57238710a8e4fe68c07d78a3eca788516e4a2b
rexsybimatw <rexsy.bima@mail.com>
2026-05-15 23:34:03 +0700

    add read_csv wip function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 use rand;
 use std::io;
+use std::{error::Error, process};
+
+use serde::Deserialize;
+
+use serde::de::DeserializeOwned;
+
+#[derive(Deserialize, Debug)]
+struct Weapon {
+    #[serde(rename = "placeholder")] // CSV column name
+    name: String, // your struct field name
+
+    #[serde(rename = "placeholder")] // CSV column name
+    description: String,
+}
+
+fn read_csv<T: DeserializeOwned>(path: &str) -> Result<Vec<T>, Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_path(path)?;
+    let items: Result<Vec<T>, _> = rdr.deserialize().collect();
+    let items = items?;
+    Ok(items)
+}
 
 fn main() {
+    todo!("get the field header of the csv file");
+    let csv_file_paths = ["location1", "location2"];
     let app_version: u8 = 1;
     let gamma_tip = "Use bandage to stop the bleeding";
     let gamma_tips = vec!["tip 1", "tip 2", "tip 3"];
